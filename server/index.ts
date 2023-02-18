c291b0a28b3a9b67fd945dbd1643851fcedd6fd5
sc30gsw <haidoushi875@gmail.com>
2023-02-18 15:21:17 +0900

    DB接続設定

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 // Express FWによるローカルサーバーの立ち上げ
 const app: express.Express = express();
 const PORT = 4000;
+const url = process.env.MONGODB_URL ? process.env.MONGODB_URL : "";
 
-app.get("/", (req: express.Request, res: express.Response) => {
-	res.send("Hello Express");
-});
+// DB接続
+try {
+	mongoose.set("strictQuery", true);
+	mongoose.connect(url);
+	console.log("DB接続");
+} catch (e) {
+	console.log(e);
+}
 
 app.listen(PORT, () => {
 	console.log("ローカルサーバー起動中");
