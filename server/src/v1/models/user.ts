f17dbfaba634634412aea74f4e50b11b0de9215e
sc30gsw <haidoushi875@gmail.com>
2023-02-18 15:02:25 +0900

    Userモデルの作成

---
+++ server/src/v1/models/user.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+// userModel作成
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+});
+
+module.exports = mongoose.model("User", userSchema);
