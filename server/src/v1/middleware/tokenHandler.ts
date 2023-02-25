fac8e3138f1243f621a74c45ce38a135c31de590
sc30gsw <haidoushi875@gmail.com>
2023-02-25 23:48:22 +0900

    JWTトークンを復号し、ユーザー情報を返却するミドルウェア関数の作成

--- server/src/v1/middleware/tokenHandler.ts
+++ server/src/v1/middleware/tokenHandler.ts
@@ -1,23 +1,36 @@
 import express, { NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-// クライアントから渡されたJWTが正常か検証
+const User = require("../models/user");
+
+// express.Requestに拡張でuser型を追加
+declare global {
+	namespace Express {
+		interface Request {
+			user?: { id: string };
+		}
+	}
+}
+
+// JWTトークンを復号する処理
 const tokenDecode = (req: express.Request) => {
 	// リクエストヘッダーの"authorization"を取得
-	const bearerHeader = req.headers["authorization"];
+	const bearerHeader = req.headers.authorization;
 
-	// bearerHeaderが存在する場合
+	// 認証情報が存在する場合
 	if (bearerHeader) {
 		// トークンを取得
 		const bearer = bearerHeader.split(" ")[1];
+
 		try {
 			// トークンを復号
-			const secret = process.env.TOKEN_SECRET_KEY
-				? process.env.TOKEN_SECRET_KEY
-				: "";
-			const decodedToken = jwt.verify(bearer, secret);
+			const decodedToken = jwt.verify(
+				bearer,
+				process.env.TOKEN_SECRET_KEY as string
+			);
+
 			return decodedToken;
-		} catch (e) {
+		} catch {
 			return false;
 		}
 	} else {
@@ -26,10 +39,30 @@ const tokenDecode = (req: express.Request) => {
 };
 
 // JWTを検証するためのミドルウェア
-const vefiryToken = (
+const verifyToken = async (
 	req: express.Request,
 	res: express.Response,
 	next: NextFunction
-) => {};
+) => {
+	// 復号したトークンを取得
+	const decodedToken = tokenDecode(req);
 
-export { vefiryToken };
+	// トークンが存在する場合
+	if (decodedToken) {
+		// ユーザーを取得（トークンはもともとユーザーのIDから生成したものであるため検索可能）
+		const user = await User.findById((decodedToken as any).id);
+
+		// ユーザーが存在しない場合
+		if (!user) {
+			return res.status(401).json("権限がありません");
+		}
+
+		// リクエスト情報を取得したユーザーで上書き
+		req.user = user;
+		next();
+	} else {
+		return res.status(401).json("権限がありません");
+	}
+};
+
+export { verifyToken };
