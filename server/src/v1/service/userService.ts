da97dd1b661aee8ae48435349873391c286e1147
sc30gsw <haidoushi875@gmail.com>
2023-02-25 23:47:05 +0900

    三項演算子による記述を修正

--- server/src/v1/service/userService.ts
+++ server/src/v1/service/userService.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import CryptoJS from "crypto-js";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
@@ -10,19 +10,22 @@ const register = async (req: express.Request, res: express.Response) => {
 	const password = req.body.password;
 	try {
 		// パスワードの暗号化
-		const key = process.env.SECRET_KEY ? process.env.SECRET_KEY : "";
-		req.body.password = CryptoJS.AES.encrypt(password, key);
+		req.body.password = CryptoJS.AES.encrypt(
+			password,
+			process.env.SECRET_KEY as string
+		);
 
 		// ユーザー新規作成
 		const user = await User.create(req.body);
 
 		// JWTの発行
-		const secret = process.env.TOKEN_SECRET_KEY
-			? process.env.TOKEN_SECRET_KEY
-			: "";
-		const token = jwt.sign({ id: user._id }, secret, {
-			expiresIn: "24h",
-		});
+		const token = jwt.sign(
+			{ id: user._id },
+			process.env.TOKEN_SECRET_KEY as string,
+			{
+				expiresIn: "24h",
+			}
+		);
 
 		return res.status(200).json({ user, token });
 	} catch (e) {
@@ -48,12 +51,9 @@ const login = async (req: express.Request, res: express.Response) => {
 
 		// パスワードが合致するか照合
 		// →新規登録時に暗号化したパスワードを復号化することで照合可能
-		const secret = process.env.TOKEN_SECRET_KEY
-			? process.env.TOKEN_SECRET_KEY
-			: "";
 		const decryptedPassword = CryptoJS.AES.decrypt(
 			user.password,
-			secret
+			process.env.TOKEN_SECRET_KEY as string
 		).toString(CryptoJS.enc.Utf8);
 
 		// 入力したパスワードが復号化したパスワードと一致しない場合
@@ -67,9 +67,13 @@ const login = async (req: express.Request, res: express.Response) => {
 		}
 
 		// JWTトークンの発行
-		const token = jwt.sign({ id: user._id }, secret, {
-			expiresIn: "24h",
-		});
+		const token = jwt.sign(
+			{ id: user._id },
+			process.env.TOKEN_SECRET_KEY as string,
+			{
+				expiresIn: "24h",
+			}
+		);
 
 		return res.status(201).json({ user, token });
 	} catch (e) {
