19d38ef1ac8e829d5f652b3871256d894eb468dd
sc30gsw <haidoushi875@gmail.com>
2023-03-12 16:25:53 +0900

    ルーティングの設定

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import AuthLayout from "./components/layout/AuthLayout";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
 
-function App() {
-	return <div className="App"></div>;
-}
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<AuthLayout />}>
+					<Route path="login" element={<Login />}></Route>
+					<Route path="register" element={<Register />}></Route>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
 export default App;
