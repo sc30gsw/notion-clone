548f4846fc13cce1f337c9fe94f3afb79dc3a473
sc30gsw <haidoushi875@gmail.com>
2023-03-22 13:59:56 +0900

    ThemeProviderの追加

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,24 @@ import "./App.css";
 import AuthLayout from "./components/layout/AuthLayout";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
 const App = () => {
+	const theme = createTheme({
+		palette: { primary: { main: "#0000ff" } },
+	});
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<AuthLayout />}>
-					<Route path="login" element={<Login />}></Route>
-					<Route path="register" element={<Register />}></Route>
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<AuthLayout />}>
+						<Route path="login" element={<Login />}></Route>
+						<Route path="register" element={<Register />}></Route>
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</ThemeProvider>
 	);
 };
 
