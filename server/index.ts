35c9c7d9517c2928e8a335d89f5a127f024c1024
sc30gsw <haidoushi875@gmail.com>
2023-03-22 15:35:51 +0900

    フロント側との接続時にcorsエラーが出力するため、ポート番号3000を許可するよう修正

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,18 @@
 import express, { Router } from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
+const cors = require("cors");
+
+const app: express.Express = express();
+
+// CORS対応
+app.use(
+	cors({
+		origin: "http://loclhost:3000",
+	})
+);
 
 // Express FWによるローカルサーバーの立ち上げ
-const app: express.Express = express();
 const PORT = 4000;
 const url = process.env.MONGODB_URL ? process.env.MONGODB_URL : "";
 
