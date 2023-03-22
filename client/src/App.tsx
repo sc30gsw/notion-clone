46688dddef846d472a2b7b106c5a58b10db20cb6
sc30gsw <haidoushi875@gmail.com>
2023-03-22 18:15:59 +0900

    ルーティング機能の実装

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import AuthLayout from "./components/layout/AuthLayout";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import AppLayout from "./components/layout/AppLayout";
+import Home from "./components/pages/Home";
 
 const App = () => {
 	const theme = createTheme({
@@ -17,6 +19,10 @@ const App = () => {
 					<Route path="/" element={<AuthLayout />}>
 						<Route path="login" element={<Login />}></Route>
 						<Route path="register" element={<Register />}></Route>
+					</Route>
+					<Route path="/" element={<AppLayout />}>
+						<Route index element={<Home />} />
+						<Route path="memo" element={<Home />} />
 					</Route>
 				</Routes>
 			</BrowserRouter>
