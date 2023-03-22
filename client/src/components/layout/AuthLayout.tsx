f749f9e8017ec282721aecad40bdde8e52e992e7
sc30gsw <haidoushi875@gmail.com>
2023-03-22 14:00:26 +0900

    共通部品の作成

---
+++ client/src/components/layout/AuthLayout.tsx
@@ -0,0 +1,31 @@
+import { Box, Container } from "@mui/system";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import notionLogo from "../../assets/images/notion-logo.png";
+
+const AuthLayout = () => {
+	return (
+		<div>
+			<Container component="main" maxWidth="xs">
+				<Box
+					sx={{
+						marginTop: 6,
+						display: "flex",
+						alignItems: "center",
+						flexDirection: "column",
+					}}
+				>
+					<img
+						src={notionLogo}
+						alt=""
+						style={{ width: 100, height: 100, marginBottom: 3 }}
+					/>
+					Notionクローン開発
+				</Box>
+			</Container>
+			<Outlet />
+		</div>
+	);
+};
+
+export default AuthLayout;
