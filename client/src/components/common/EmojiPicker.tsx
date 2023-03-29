370cf346feabc79dcbf1d85ab88be21e027d3cb5
sc30gsw <haidoushi875@gmail.com>
2023-03-29 18:51:03 +0900

    アイコンのスタイリング

--- client/src/components/common/EmojiPicker.tsx
+++ client/src/components/common/EmojiPicker.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useEffect, useState } from "react";
 
 type EmojiPickerProps = {
 	icon: string;
 };
 
 const EmojiPicker = ({ icon }: EmojiPickerProps) => {
-	return <div>{icon}</div>;
+	const [selectedEmoji, setSelectedEmoji] = useState<string>("");
+
+	useEffect(() => {
+		setSelectedEmoji(icon);
+	}, [icon]);
+
+	return (
+		<Box>
+			<Typography variant="h3" fontWeight={700} sx={{ cursor: "pointer" }}>
+				{selectedEmoji}
+			</Typography>
+		</Box>
+	);
 };
 
 export default EmojiPicker;
