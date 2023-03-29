637bf81c4538614965fd2787522772c6b9e54e38
sc30gsw <haidoushi875@gmail.com>
2023-03-29 18:59:46 +0900

    絵文字ピッカーの表示・非表示処理の追加

--- client/src/components/common/EmojiPicker.tsx
+++ client/src/components/common/EmojiPicker.tsx
@@ -1,6 +1,7 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
+import Picker from "@emoji-mart/react";
 
 type EmojiPickerProps = {
 	icon: string;
@@ -8,16 +9,33 @@ type EmojiPickerProps = {
 
 const EmojiPicker = ({ icon }: EmojiPickerProps) => {
 	const [selectedEmoji, setSelectedEmoji] = useState<string>("");
+	const [isShowPicker, setIsShowPicker] = useState<boolean>(false);
 
 	useEffect(() => {
 		setSelectedEmoji(icon);
 	}, [icon]);
 
+	const showPicker = () => setIsShowPicker(!isShowPicker);
+
 	return (
 		<Box>
-			<Typography variant="h3" fontWeight={700} sx={{ cursor: "pointer" }}>
+			<Typography
+				variant="h3"
+				fontWeight={700}
+				sx={{ cursor: "pointer" }}
+				onClick={showPicker}
+			>
 				{selectedEmoji}
 			</Typography>
+			<Box
+				sx={{
+					display: isShowPicker ? "block" : "none",
+					position: "absolute",
+					zIndex: 100,
+				}}
+			>
+				<Picker />
+			</Box>
 		</Box>
 	);
 };
