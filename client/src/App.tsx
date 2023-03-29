529bd4162bc49e9dc2e22b1a26a448c362fe4765
sc30gsw <haidoushi875@gmail.com>
2023-03-29 15:39:11 +0900

    ルーティング設定

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import Register from "./components/pages/Register";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import AppLayout from "./components/layout/AppLayout";
 import Home from "./components/pages/Home";
+import Memo from "./components/pages/Memo";
 
 const App = () => {
 	const theme = createTheme({
@@ -23,6 +24,7 @@ const App = () => {
 					<Route path="/" element={<AppLayout />}>
 						<Route index element={<Home />} />
 						<Route path="memo" element={<Home />} />
+						<Route path="memo/:memoId" element={<Memo />} />
 					</Route>
 				</Routes>
 			</BrowserRouter>
